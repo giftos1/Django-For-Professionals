9a19e374ecc52e31e7f77287d6699abeec320fcf
giftos1 <giftsydney676@gmail.com>
2024-12-28 10:23:31 +1000

    ch10-Email

--- ch4-bookstore/bookstore_project/settings.py
+++ ch4-bookstore/bookstore_project/settings.py
@@ -153,3 +153,4 @@ ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False  # remove confirm password
 ACCOUNT_USERNAME_REQUIRED = False  # remove username
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
+DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
