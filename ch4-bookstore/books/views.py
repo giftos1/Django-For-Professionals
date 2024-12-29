35c72a657e2a4dcb6df7569171eb6f49b47ce180
giftos1 <giftsydney676@gmail.com>
2024-12-29 15:19:56 +1000

    ch15-Search

--- ch4-bookstore/books/views.py
+++ ch4-bookstore/books/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import (
     LoginRequiredMixin, PermissionRequiredMixin
 )
+from django.db.models import Q
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
@@ -21,3 +22,18 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template = 'books/book_detail.html'
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'book_results_list'
+    template_name = 'books/search_results.html'
+
+    # queryset = Book.objects.filter(title__icontains='professionals')
+
+    def get_queryset(self):
+        query = self.request.GET.get("search")
+        # The Q helps us use & | operators for filtering
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(title__icontains=query)
+        )
