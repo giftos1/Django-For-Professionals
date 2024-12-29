ba61e5d8331b09ba92bcd0ecb290c6be8cc03382
giftos1 <giftsydney676@gmail.com>
2024-12-29 13:54:17 +1000

    ch14-Permissions

--- ch4-bookstore/books/tests.py
+++ ch4-bookstore/books/tests.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-
+from django.contrib.auth.models import Permission
+from django.urls import reverse
 from books.models import Book, Review
 
 
@@ -15,7 +16,11 @@ class BookTests(TestCase):
             price="25.00"
         )
 
-        cls.user = get_user_model().objects.create(
+        cls.special_permission = Permission.objects.get(
+            codename="special_status"
+        )
+
+        cls.user = get_user_model().objects.create_user(
             username="reviewuser",
             email="reviewuser@email.com",
             password="testpass123",
@@ -32,17 +37,32 @@ class BookTests(TestCase):
         self.assertEqual(self.book.author, "JK Rowling")
         self.assertEqual(self.book.price, "25.00")
 
-    def test_book_list_view(self):
-        response = self.client.get('/books/')
+    def test_book_detail_view_for_logged_in_user(self):
+        self.client.login(email="reviewuser@email.com", password="testpass123", )
+        response = self.client.get(reverse("book_list"))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Harry Potter')
-        self.assertTemplateUsed(response, 'books/book_list.html')
+        self.assertContains(response, "Harry Potter")
+        self.assertTemplateUsed(response, "books/book_list.html")
 
-    def test_book_detail_view(self):
+    def test_book_detail_view_for_logged_out_user(self):
+        self.client.logout()
+        response = self.client.get(reverse("book_list"))
+        self.assertEqual(response.status_code, 302)  # new
+        self.assertRedirects(
+            response, "%s?next=/books/" % (reverse("account_login")))
+        response = self.client.get(
+            "%s?next=/books/" % (reverse("account_login")))
+        self.assertContains(response, "Log In")
+
+    def test_book_detail_view_with_permissions(self):
+        self.client.login(email="reviewuser@email.com", password="testpass123", )
+        self.user.user_permissions.add(
+            self.special_permission
+        )
         response = self.client.get(self.book.get_absolute_url())
-        no_response = self.client.get('/books/456775')
+        no_response = self.client.get("/books/12345/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
-        self.assertContains(response, 'Harry Potter')
-        self.assertContains(response, 'An excellent review')
-        self.assertTemplateUsed(response, 'books/book_detail.html')
+        self.assertContains(response, "Harry Potter")
+        self.assertContains(response, "An excellent review")
+        self.assertTemplateUsed(response, "books/book_detail.html")
--- ch4-bookstore/books/views.py
+++ ch4-bookstore/books/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin, PermissionRequiredMixin
+)
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
@@ -5,13 +8,16 @@ from books.models import Book
 
 
 # Create your views here.
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     context_object_name = 'books'
     template = 'books/book_list.html'
+    login_url = 'account_login'
 
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Book
     context_object_name = 'book'
     template = 'books/book_detail.html'
+    login_url = 'account_login'
+    permission_required = 'books.special_status'
